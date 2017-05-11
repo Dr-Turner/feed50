f1c290b2bba7f7fa23ad730825c9046cbc3517ef
Dave Turner <D_Turner_Dev@Outlook.com>
2017-05-11 19:04:59 +0100

    Switched to production mode

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from tempfile import mkdtemp
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app/db/test.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app/db/production.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False    # to disable annoying warnings
 SESSION_FILE_DIR = mkdtemp()
 SESSION_PERMANENT = False
