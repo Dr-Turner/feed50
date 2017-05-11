33c91e5b2e374c6184db50e4c2f1d4894a72209d
Dave Turner <D_Turner_Dev@Outlook.com>
2017-05-11 21:04:43 +0100

    Included blank db for production

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from config import db_path
+import os
 
 app = Flask(__name__)
 app.config.from_object("config")
 
+
 db = SQLAlchemy(app)
 Session(app)
+
+# set up date base if it doesn't exist
+if not os.path.exists(db_path):
+    db.create_all()
+
 
 # module level import need to be here to avoid a pesky cicular import.
 from app import views
--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import os
 from tempfile import mkdtemp
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'app/db/production.db')
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app/db/production.db')
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path
 SQLALCHEMY_TRACK_MODIFICATIONS = False    # to disable annoying warnings
 SESSION_FILE_DIR = mkdtemp()
 SESSION_PERMANENT = False
